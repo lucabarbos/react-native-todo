28b4103279b67d528ebce2fe1612182b338d9858
Lucas Barbosa <contato@lucasbarbosa.dev>
2022-09-25 21:59:47 -0300

    :bug: Resolução de desafio de conceitos

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,46 @@
-import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
 
-import { Header } from '../components/Header';
-import { Task, TasksList } from '../components/TasksList';
-import { TodoInput } from '../components/TodoInput';
+import { Header } from "../components/Header";
+import { Task, TasksList } from "../components/TasksList";
+import { TodoInput } from "../components/TodoInput";
 
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
     //TODO - add new task
+
+    const task = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false,
+    };
+
+    setTasks((oldState) => [...oldState, task]);
   }
 
   function handleToggleTaskDone(id: number) {
     //TODO - toggle task done if exists
+
+    const changeTaskStatus = tasks.map((task) =>
+      task.id === id
+        ? {
+            ...task,
+            done: !task.done,
+          }
+        : task
+    );
+
+    setTasks(changeTaskStatus);
   }
 
   function handleRemoveTask(id: number) {
     //TODO - remove task from state
+
+    const removeTask = tasks.filter((task) => task.id !== id);
+
+    setTasks(removeTask);
   }
 
   return (
@@ -26,18 +49,18 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <TasksList 
-        tasks={tasks} 
+      <TasksList
+        tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
+        removeTask={handleRemoveTask}
       />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#EBEBEB'
-  }
-})
+    backgroundColor: "#EBEBEB",
+  },
+});
